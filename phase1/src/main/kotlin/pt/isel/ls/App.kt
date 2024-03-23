d69d23d0374d59ac2710b3672eefd5f73a97195c
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-03-23 20:11:32 +0000

    buildRoutes and routingHttpHandler and Server  implementation

--- phase1/src/main/kotlin/pt/isel/ls/App.kt
+++ phase1/src/main/kotlin/pt/isel/ls/App.kt
@@ -1,5 +1,18 @@
 package pt.isel.ls
 
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
+
+private const val ENV_NAME = "JDBC_DATABASE_URL"
+
 fun main() {
-    println("Hello LS")
+    val routes = routingHttpHandler(ENV_NAME)
+
+    val server = routes.asServer(Jetty(8080)).start()
+    println("Server started at ${server.port()}")
+
+    readln()
+    server.stop()
+
+    println("Server stopped")
 }
---
+++ phase1/src/main/kotlin/pt/isel/ls/Utils.kt
@@ -0,0 +1,34 @@
+package pt.isel.ls
+
+import org.http4k.routing.RoutingHttpHandler
+import pt.isel.ls.server.buildRoutes
+import pt.isel.ls.services.GameManagement
+import pt.isel.ls.services.PlayerManagement
+import pt.isel.ls.services.SessionManagement
+import pt.isel.ls.storage.GameStorage
+import pt.isel.ls.storage.PlayerStorage
+import pt.isel.ls.storage.SessionStorage
+import pt.isel.ls.webApi.GameHandler
+import pt.isel.ls.webApi.PlayerHandler
+import pt.isel.ls.webApi.SessionHandler
+
+/**
+ * Builds the routing HTTP handler for the application services.
+ *
+ * @param envName the environment variable name
+ * @return the routing HTTP handler for the application services
+ * @see RoutingHttpHandler
+ */
+internal fun routingHttpHandler(envName: String): RoutingHttpHandler {
+    val playerStorage = PlayerStorage(envName)
+    val gameStorage = GameStorage(envName)
+    val sessionStorage = SessionStorage(envName)
+    val playerServices = PlayerManagement(playerStorage)
+    val gameServices = GameManagement(gameStorage)
+    val sessionServices = SessionManagement(sessionStorage, playerStorage)
+    val playerHandler = PlayerHandler(playerServices)
+    val gameHandler = GameHandler(gameServices)
+    val sessionHandler = SessionHandler(sessionServices)
+    val routes = buildRoutes(playerHandler, gameHandler, sessionHandler)
+    return routes
+}

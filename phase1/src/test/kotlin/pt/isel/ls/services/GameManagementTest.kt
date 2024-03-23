30ba8610918130a5de97c2ec32771f37af8a47b5
GuilhermeBelo2904 <116506659+GuilhermeBelo2904@users.noreply.github.com>
2024-03-23 11:50:22 +0000

    Game Management Test and Game Handler Test restructuring.

--- phase1/src/test/kotlin/pt/isel/ls/services/GameManagementTest.kt
+++ phase1/src/test/kotlin/pt/isel/ls/services/GameManagementTest.kt
@@ -8,56 +8,94 @@ import kotlin.test.assertFailsWith
 import kotlin.test.assertTrue
 
 class GameManagementTest {
-    private fun makeGameManagementTest(code: (gameServices: GameServices) -> Unit) {
-        GameManagement((GameStorageStunt())).run { code(this) }
-    }
+    private fun executeGameManagementTest(action: (gameServices: GameServices) -> Unit) =
+        GameManagement(GameStorageStunt())
+            .run(action)
 
     @Test
-    fun `create a new Game`() {
-        makeGameManagementTest {
-            assertEquals(4u, it.createGame("test", "dev", setOf("genre")))
-            assertEquals(5u, it.createGame("test", "dev", setOf("genre")))
+    fun `create a new Game - Success`() {
+        executeGameManagementTest { handler ->
+            // ARRANGE
+            val expectedGid1 = 4u
+            val expectedGid2 = 5u
+
+            // ACT
+            val game1 = handler.createGame("test", "dev", setOf("genre"))
+            val game2 = handler.createGame("test", "dev", setOf("genre"))
+
+            // ASSERT
+            assertEquals(expectedGid1, game1)
+            assertEquals(expectedGid2, game2)
         }
     }
 
     @Test
-    fun `error creating a game`() {
-        makeGameManagementTest {
-            assertFailsWith<ServicesError> { it.createGame("test", "dev", setOf()) }
-        }
-    }
+    fun `error creating a game - Failure due to invalid parameter`() {
+        executeGameManagementTest { handler ->
+            // ARRANGE
+            val gameName = "test"
+            val gameDev = "dev"
+            val invalidGenres = setOf<String>()
 
-    @Test
-    fun `get details of a game`() {
-        makeGameManagementTest {
-            val game = it.getGameDetails(1u)
-            assertEquals("test", game.name)
-            assertEquals("dev", game.dev)
-            assertTrue { "genre" in game.genres }
-        }
-    }
-
-    @Test
-    fun `error getting details of a game`() {
-        makeGameManagementTest {
-            assertFailsWith<ServicesError> { it.getGameDetails(60u) }
-        }
-    }
-
-    @Test
-    fun `get Game by developer and genres`() {
-        makeGameManagementTest {
-            val games = it.getGameByDevAndGenres(dev = "dev", genres = setOf("genre"), null, null)
-            assertEquals(2, games.size)
-        }
-    }
-
-    @Test
-    fun `get Game by developer and genres with no results`() {
-        makeGameManagementTest {
+            // ACT & ASSERT
             assertFailsWith<ServicesError> {
-                it.getGameByDevAndGenres(dev = "dev", genres = setOf("genre2"), null, null)
+                handler.createGame(gameName, gameDev, invalidGenres)
             }
+        }
+    }
+
+    @Test
+    fun `get details of a game - Success`() {
+        executeGameManagementTest { handler ->
+            // ARRANGE
+            val expectedName = "test"
+            val expectedDev = "dev"
+            val expectedGenre = "genre"
+
+            // ACT
+            val game = handler.getGameDetails(1u)
+
+            // ASSERT
+            assertEquals(expectedName, game.name)
+            assertEquals(expectedDev, game.dev)
+            assertTrue { expectedGenre in game.genres }
+        }
+    }
+
+    @Test
+    fun `error getting details of a game - Failure due to non-existing game id`() {
+        executeGameManagementTest { handler ->
+            // ARRANGE
+            val invalidGid = 60u
+
+            // ACT & ASSERT
+            assertFailsWith<ServicesError> { handler.getGameDetails(invalidGid) }
+        }
+    }
+
+    @Test
+    fun `get Game by developer and genres - Success`() {
+        executeGameManagementTest { handler ->
+            // ARRANGE
+            val expectedSize = 2
+
+            // ACT
+            val games = handler.getGameByDevAndGenres("dev", setOf("genre"))
+
+            // ASSERT
+            assertEquals(expectedSize, games.size)
+        }
+    }
+
+    @Test
+    fun `get Game by developer and genres with no results - Failure due to non existing combination of developer and genres`() {
+        executeGameManagementTest { handler ->
+            // ARRANGE
+            val gameDev = "dev"
+            val gameGenres = setOf("genre2")
+
+            // ACT & ASSERT
+            assertFailsWith<ServicesError> { handler.getGameByDevAndGenres(gameDev, gameGenres) }
         }
     }
 }

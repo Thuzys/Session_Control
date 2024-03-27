1b3f872c7df8bab38b3e8761f30f135e4fdcc013
Brgm37 <brgm37@gmail.com>
2024-03-27 15:57:09 +0000

    Revert "merge commit."
    This reverts commit 0f555c19e3ab7b6da80e13a9abf148186301be9f.

--- phase1/src/main/sql/addData.sql
+++ phase1/src/main/sql/addData.sql
@@ -8,22 +8,10 @@ INSERT INTO GAME (name, developer) VALUES ('Game 1', 'Developer A'), ('Game 2', 
 INSERT INTO GAME_GENRES (gid, genre) VALUES (1, 'Action'), (1, 'Adventure'), (2, 'RPG'), (3, 'Simulation');
 
 -- Insert values into PLAYER table
-INSERT INTO PLAYER (name, email, token) VALUES ('Player 1', 'player1@example.com', 'e247758f-02b6-4037-bd85-fc245b84d5f2'), ('Player 2', 'player2@example.com', 'e247758f-02b6-4037-bd85-fc245b84d5f2');
+INSERT INTO PLAYER (name, email, token) VALUES ('Player 1', 'player1@example.com', 'token1'), ('Player 2', 'player2@example.com', 'token2');
 
 -- Insert values into SESSION table
 INSERT INTO SESSION (capacity, gid, date) VALUES (10, 1, '2024-03-11T12:30'), (8, 2, '2024-03-11T12:30'), (12, 3, '2024-03-13T12:30');
 
 -- Insert values into PLAYER_SESSION table
 INSERT INTO PLAYER_SESSION (pid, sid) VALUES (1, 1), (2, 2), (1, 3), (2, 3);
-
-SELECT s.sid, s.capacity, s.gid, s.date
-FROM session s
-         JOIN player_session ps ON ps.sid = s.sid
-WHERE (s.gid = ?)
-   OR (s.date = ?)
-   OR (ps.pid = ?)
-GROUP BY s.sid, s.capacity, s.gid, s.date
-HAVING (:state IS NULL)
-    OR (:state = 'OPEN' AND s.capacity > count(ps.pid))
-    OR (:state = 'CLOSE' AND s.capacity = count(ps.pid))
-OFFSET ? LIMIT ?;

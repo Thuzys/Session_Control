d73266bc7f79c1344a7b739f13f131bf599cbd89
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-03-27 17:45:15 +0000

    add route.

--- phase2/src/test/kotlin/pt/isel/ls/server/RoutesKtTest.kt
+++ phase2/src/test/kotlin/pt/isel/ls/server/RoutesKtTest.kt
@@ -105,4 +105,14 @@ class RoutesKtTest {
                 "No matching handler found for $request",
             )
         }
+
+    @Test
+    fun `buildRoutes returns router with get SESSION_PLAYER_ROUTE`() =
+        actionOfRoutesArrangement { handler: RoutingHttpHandler ->
+            val request = Request(Method.DELETE, SESSION_PLAYER_ROUTE)
+            assertIs<RouterMatch.MatchingHandler>(
+                handler.match(request),
+                "No matching handler found for $request",
+            )
+        }
 }

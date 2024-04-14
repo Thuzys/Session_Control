530a8a47ab85e8471fc71f2b619e1adddffe2469
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-04-14 15:55:52 +0100

    executeCommandWithResponse update.

--- phase2/static-content/handlers/handlerUtils/handlerUtils.js
+++ phase2/static-content/handlers/handlerUtils/handlerUtils.js
@@ -22,7 +22,7 @@ function executeCommandWithResponse(url, responseHandler) {
             if(isResponseOK(response)) {
                 responseHandler(response);
             } else {
-
+                window.history.back()
                 response.text().then(text => alert("Error fetching data: " + text));
             }
         })

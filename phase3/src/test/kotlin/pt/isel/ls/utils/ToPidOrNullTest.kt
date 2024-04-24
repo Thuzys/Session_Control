b6144e2e81f15a4f3d67e69f0132309e13b26ea8
Arthur Oliveira <arthurcnoliveira@gmail.com>
2024-04-24 17:11:01 +0100

    ToPidOuNull, UnauthorizedAccess and PlayerHandler Tests update.

--- phase3/src/test/kotlin/pt/isel/ls/utils/ToPidOrNullTest.kt
+++ phase3/src/test/kotlin/pt/isel/ls/utils/ToPidOrNullTest.kt
@@ -2,6 +2,9 @@ package pt.isel.ls.utils
 
 import org.http4k.core.Method
 import org.http4k.core.Request
+import org.http4k.core.UriTemplate
+import org.http4k.routing.RequestWithRoute
+import org.http4k.routing.RoutedRequest
 import pt.isel.ls.webApi.toPidOrNull
 import kotlin.test.Test
 import kotlin.test.assertNotNull
@@ -12,14 +15,21 @@ private const val DUMMY_ROUTE = "/dummyRoute"
 class ToPidOrNullTest {
     @Test
     fun `test with a valid pid in request`() {
-        val request = Request(Method.GET, DUMMY_ROUTE)
-            .header("pid", "1")
+        val request =
+            RoutedRequest(
+                Request(Method.GET, "$DUMMY_ROUTE/1"),
+                UriTemplate.from("$DUMMY_ROUTE/{pid}"),
+            )
         assertNotNull(request.toPidOrNull())
     }
 
     @Test
     fun `test with an invalid pid in request`() {
-        val request = Request(Method.GET, DUMMY_ROUTE)
+        val request =
+            RoutedRequest(
+                Request(Method.GET, "$DUMMY_ROUTE/invalid"),
+                UriTemplate.from("$DUMMY_ROUTE/{pid}"),
+            )
         assertNull(request.toPidOrNull())
     }
 }

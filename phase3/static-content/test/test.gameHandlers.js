df0ba34665f7d5fab125feba40f2a1117593e2e9
GuilhermeBelo2904 <116506659+GuilhermeBelo2904@users.noreply.github.com>
2024-05-12 20:59:16 +0100

    front end test correction

--- phase3/static-content/test/test.gameHandlers.js
+++ phase3/static-content/test/test.gameHandlers.js
@@ -1,43 +1,58 @@
 import gameHandlers from "../handlers/gameHandlers.js";
+import views from "../views/viewsCreators.js";
 
 describe('Test gameHandlers', function() {
-    function simulateGameSearchFormSubmission(mainContentChildren, dev, genres) {
+    function simulateGameSearchFormSubmission(mainContentChildren, dev, name, genres) {
         const e = new Event('submit');
         window.document.getElementById = function(id) {
             switch (id) {
+                case 'InputName': return { value: name };
                 case 'InputDev': return { value: dev };
                 case 'ul': return { children: genres };
                 default: return null;
             }
         };
-        mainContentChildren[1].dispatchEvent(e);
+        mainContentChildren[0].dispatchEvent(e);
     }
 
-    it('searchGames should alter the mainContent and headerContent correctly', function () {
+    it('searchGames should alter the mainContent correctly', function () {
         const mainContent = document.createElement("div")
-        const headerContent = document.createElement("div")
 
-        gameHandlers.searchGames(mainContent, headerContent)
+        gameHandlers.searchGames(mainContent)
 
         const mainContentChildren = mainContent.children
-        mainContentChildren.length.should.equal(2)
-        mainContentChildren[0].tagName.should.equal("H1")
-        mainContentChildren[1].tagName.should.equal("FORM")
-        mainContentChildren[1].onsubmit.should.not.equal(null)
-
-        const headerContentChildren = headerContent.children
-        headerContentChildren.length.should.equal(3)
-        headerContentChildren[0].tagName.should.equal("A")
+        mainContentChildren.length.should.equal(1)
+        mainContentChildren[0].tagName.should.equal("DIV")
+        const divChildren = mainContentChildren[0].children
+        divChildren.length.should.equal(2)
     });
 
     it('searchGames should change the hash correctly', function () {
         const mainContent = document.createElement("div")
-        const headerContent = document.createElement("div")
 
-        gameHandlers.searchGames(mainContent, headerContent)
-        const genres = [document.createElement("div"), document.createElement("div")]
-        simulateGameSearchFormSubmission(mainContent.children, 'dev', genres)
+        gameHandlers.searchGames(mainContent)
+        simulateGameSearchFormSubmission(
+            mainContent.children,
+            'dev',
+            'name',
+            views.ul([views.li('genre1'), views.li('genre2')]).children
+        )
 
-        window.location.hash.should.equal('#games?dev=dev&genres=%2C&offset=0');
+        window.location.hash.should.equal('#games?dev=dev&name=name&genres=undefined,undefined&offset=0');
     });
+
+    it('createGame should change the hash correctly', function () {
+        const inputName = document.createElement("input")
+        const inputDev = document.createElement("input")
+        const selectedGenresView = document.createElement("div")
+        const mainContent = document.createElement("div")
+
+        inputName.value = 'name'
+        inputDev.value = 'dev'
+        selectedGenresView.children = [document.createElement("div"), document.createElement("div")]
+
+        gameHandlers.createGame(inputName, inputDev, selectedGenresView, mainContent)
+
+        window.location.hash.should.equal('#games/undefined');
+    })
 });
--- phase3/static-content/test/test.handlerViews.js
+++ phase3/static-content/test/test.handlerViews.js
@@ -1,62 +1,60 @@
 import handlerViews from "../views/handlerViews/handlerViews.js";
+import views from "../views/viewsCreators.js";
 
 describe('Test handlerViews', function() {
     it('should create a header', function() {
         const header = handlerViews.createHeader("Home page");
-        header.tagName.should.equal("H1");
+        header.tagName.should.equal("H2");
         header.textContent.should.equal("Home page");
     })
     it('should create a LabelledInput', function() {
-      const labelledInput = handlerViews.createLabeledInput("label", "text", "id");
+        const labelledInput = handlerViews.createLabeledInput("id", "text", "id");
 
-      labelledInput.length.should.equal(3);
-      labelledInput[0].tagName.should.equal("LABEL");
-      labelledInput[0].textContent.should.equal("label");
-      labelledInput[1].tagName.should.equal("INPUT");
-      labelledInput[1].type.should.equal("text");
-      labelledInput[1].id.should.equal("id");
-      labelledInput[2].tagName.should.equal("P");
+        labelledInput.tagName.should.equal("INPUT");
+        labelledInput.type.should.equal("text");
+        labelledInput.id.should.equal("id");
+        labelledInput.placeholder.should.equal("text");
     })
     it('should create a href', function() {
-      const href = handlerViews.hrefConstructor("base", "id", "textBase");
+        const href = handlerViews.hrefConstructor("base", "id", "textBase");
 
-      href.length.should.equal(2);
-      href[0].tagName.should.equal("A");
-      href[0].href.should.contain("base/id");
-      href[0].textContent.should.equal("textBase");
-      href[1].tagName.should.equal("P");
+        href.length.should.equal(2);
+        href[0].tagName.should.equal("A");
+        href[0].href.should.contain("base/id");
+        href[0].textContent.should.equal("textBase");
+        href[1].tagName.should.equal("P");
     })
     it('should create a backButton', function() {
-      const backButton = handlerViews.createBackButtonView();
+        const backButton = handlerViews.createBackButtonView();
 
-      backButton.tagName.should.equal("BUTTON");
-      backButton.type.should.equal("button");
-      backButton.textContent.should.equal("Back");
+        backButton.tagName.should.equal("BUTTON");
+        backButton.type.should.equal("button");
+        backButton.textContent.should.equal("Back");
     })
     it('should create a href Button', function() {
-          const sessionsButton = handlerViews.hrefButtonView("text", "query");
+        const sessionsButton = handlerViews.hrefButtonView("text", "query");
 
-          sessionsButton.tagName.should.equal("BUTTON");
-          sessionsButton.type.should.equal("button");
-          sessionsButton.textContent.should.equal("text");
+        sessionsButton.tagName.should.equal("BUTTON");
+        sessionsButton.type.should.equal("button");
+        sessionsButton.textContent.should.equal("text");
 
-        })
+    })
     it('should create a pagination', function() {
-          const query = new Map();
-          query.set("offset", 0)
-          const pagination = handlerViews.createPagination(query, "hash", true);
+        const query = new Map();
+        query.set("offset", 0)
+        const pagination = handlerViews.createPagination(query, "hash", true);
 
-          pagination.tagName.should.equal("DIV");
-          const buttons = pagination.children;
-            buttons.length.should.equal(2);
-            buttons[0].tagName.should.equal("BUTTON");
-            buttons[0].id.should.equal("prev");
-            buttons[0].type.should.equal("button");
-            buttons[0].textContent.should.equal("<");
-            buttons[1].tagName.should.equal("BUTTON");
-            buttons[1].id.should.equal("next");
-            buttons[1].type.should.equal("button");
-            buttons[1].textContent.should.equal(">");
+        pagination.tagName.should.equal("DIV");
+        const buttons = pagination.children;
+        buttons.length.should.equal(2);
+        buttons[0].tagName.should.equal("BUTTON");
+        buttons[0].id.should.equal("prev");
+        buttons[0].type.should.equal("button");
+        buttons[0].textContent.should.equal("<");
+        buttons[1].tagName.should.equal("BUTTON");
+        buttons[1].id.should.equal("next");
+        buttons[1].type.should.equal("button");
+        buttons[1].textContent.should.equal(">");
     })
 
     it('should create custom alert', function () {
@@ -74,4 +72,24 @@ describe('Test handlerViews', function() {
         alertButton[0].click();
         alertModal.style.display.should.equal("none");
     });
+
+    it('should verify if ul has item', function () {
+        const ul = views.ul()
+        const item = views.li("item")
+        ul.appendChild(item)
+        const result = handlerViews.ulHasItem("item", ul.children);
+
+        result.should.equal(true);
+    });
+
+    it('should create a radio button', function() {
+        const radio = handlerViews.createRadioButton("open", "openState");
+
+        radio.tagName.should.equal("LABEL");
+        radio.textContent.should.equal("openState");
+        const input = radio.children[0];
+        input.tagName.should.equal("INPUT");
+        input.type.should.equal("radio");
+        input.name.should.equal("state");
+    })
 })
